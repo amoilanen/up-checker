15c745a8753b1f66ca73a13f5401006b548b687c
Anton Ivanov <anton.al.ivanov@gmail.com>
2018-02-13 23:01:01 +0000

    Add test case for inserting a group, re-factor the test

--- test/controllers/GroupControllerSpec.scala
+++ test/controllers/GroupControllerSpec.scala
@@ -2,36 +2,51 @@ package controllers
 
 import context.Context
 import daos.GroupDao
-import models.Group
+import models.{Group, GroupRequest}
 import models.GroupReadWrites._
 import org.scalatestplus.play.PlaySpec
 import play.api.mvc.{ControllerComponents, Result, Results}
 import org.mockito.Mockito._
 import org.scalatest.mockito.MockitoSugar
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
-import play.api.test.FakeRequest
+import play.api.libs.json.Json
+import play.api.test.{FakeHeaders, FakeRequest}
 import play.api.test.Helpers._
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
 class GroupControllerSpec extends PlaySpec with GuiceOneServerPerSuite with MockitoSugar {
 
   import GroupControllerSpec._
 
+  trait TestCase {
+    val dao = mock[GroupDao]
+    val controllerComponents = app.injector.instanceOf[ControllerComponents]
+    val controller = new GroupsController(dao, controllerComponents)
+  }
+
   "GroupController" should {
 
-    "should list existing groups" in {
-      val dao = mock[GroupDao]
+    "should list existing groups" in new TestCase {
       when(dao.list).thenReturn(Future.successful(existingGroups))
-      val context = app.injector.instanceOf[Context]
-      val controllerComponents = app.injector.instanceOf[ControllerComponents]
 
-      val controller = new GroupsController(dao, controllerComponents)
       val result = controller.list(FakeRequest())
 
       status(result) mustBe OK
       contentAsJson(result).validate[Seq[Group]].get mustBe existingGroups
+    }
+
+    "create a group" in new TestCase {
+      val groupRequest = GroupRequest("group1")
+      val createdGroupId = 1l
+      when(dao.create(groupRequest)).thenReturn(Future.successful(Some(createdGroupId)))
+      val request = FakeRequest(POST, "/groups", FakeHeaders(), Json.toJson(groupRequest))
+
+      val result = controller.create(request)
+
+      status(result) mustBe OK
+      contentAsJson(result).validate[Long].get mustBe createdGroupId
     }
   }
 }
